f7ff3a40f79d5b05b7c58791e88390f85b0736fe
rheins22 <127905208+rheins22@users.noreply.github.com>
2023-09-09 18:22:48 -0500

    completed support classes

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -18,8 +18,20 @@ public class PositionType {
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
 
+    public String toString() {
+        return value;
+    }
+
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PositionType)) return false;
+        PositionType position = (PositionType) o;
+        return getId() == position.getId();
+
+    }
 
     // Getters and Setters:
 
